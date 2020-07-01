131e198a192591cc82464b26edab3fd1a545e7fc
ibbocus <ib_bocus@hotmail.com>
2020-07-01 12:28:56 +0100

    lists

--- Lists.py
+++ Lists.py
@@ -8,9 +8,11 @@ print(cities)
 
 print(cities.pop(1))
 print(cities)
-cities = ["Tokyo", "Paris", "Prague", "Luxembourg"]
+cities = [["Tokyo", "Paris"], ["Prague", "Luxembourg"]]
 del cities[1]
 print(cities)
 cities.insert(4, "London")
 print(cities)
 
+other_list = [1,2,3]
+print((cities + other_list)[0])
---
+++ Tuples.py
@@ -0,0 +1,7 @@
+my_tuple = ('01/01/1996', 'London')
+my_list = list(my_tuple)
+my_list.insert(0, 'Ib')
+print(my_list)
+
+for value in my_list:
+    print(value)
