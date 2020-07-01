9f1462556235190e42d7886a9951a8b9bb9e98b2
ibbocus <ib_bocus@hotmail.com>
2020-07-01 14:41:04 +0100

    update

--- Dictionary.py
+++ Dictionary.py
@@ -9,19 +9,39 @@ student_record = {"name": "Ibrahim",
 
 rent = {'London': 5600.00, 'Paris': 3500.00, 'Tokyo': 5000.00}
 print(rent)
-total_income = 0.00
+total_income: float = 0.00
 for value in rent.values():
     total_income += value  # Accumulate the values in total_income
 print("total income:", total_income)
 v = list(rent.values())
 k = list(rent.keys())
 print("The city with the largest rent is:", k[v.index(max(v))])
-
+# Alternative method to retrieve a specific value from a key
+print("The city with the lowest rent is:", min(rent, key=rent.get))
 
 student_record = {"name": "Ibrahim",
                   "stream": "Devops",
                   "completed_lessons": 5,
                   "completed_lessons_names": ["strings", "Tuples", "variables"]}
 
-student_record["completed_lessons_names"].extend(["operators", "dictionaries", "append","extend"])
+print(student_record["completed_lessons_names"][2])
+print(student_record["name"])
+print(student_record["completed_lessons"])
+
+student_record["completed_lessons_names"].extend(["operators", "dictionaries", "append", "extend"])
 print(student_record)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
